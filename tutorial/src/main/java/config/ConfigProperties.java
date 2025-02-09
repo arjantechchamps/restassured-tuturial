1ed8b8a503219ff59e2f3dc54598e7c8d5d7005a
arjanassink <arjanassink@gmail.com>
2025-02-09 11:15:29 +0100

    implement propertie-file

---
+++ tutorial/src/main/java/config/ConfigProperties.java
@@ -0,0 +1,42 @@
+package config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ConfigProperties {
+
+    private static final Properties properties;
+    private static final Logger logger = LoggerFactory.getLogger(ConfigProperties.class);
+    private static final String DEFAULT_PROPERTIES_FILE = "config.properties";
+    private static final String ENV_VARIABLE_NAME = "CONFIG_PROPERTIES_FILE";
+
+    static {
+        properties = new Properties();
+        String propertiesFile = System.getenv(ENV_VARIABLE_NAME);
+        if (propertiesFile == null || propertiesFile.isEmpty()) {
+            propertiesFile = DEFAULT_PROPERTIES_FILE;
+        }
+
+        try (InputStream input = ConfigProperties.class.getClassLoader().getResourceAsStream(propertiesFile)) {
+            if (input == null) {
+                throw new RuntimeException("Sorry, unable to find " + propertiesFile);
+            }
+            properties.load(input);
+        } catch (IOException ex) {
+            logger.error(String.valueOf(ex));
+        }
+    }
+
+    public static String getProperty(String key) {
+        return properties.getProperty(key);
+    }
+
+    public static int getIntProperty(String key) {
+        return Integer.parseInt(properties.getProperty(key));
+    }
+}
+
---
+++ tutorial/src/main/java/io/techchamps/tutorial/F_PropertieFile/HelperWithPropertieFile.java
@@ -0,0 +1,49 @@
+package io.techchamps.tutorial.F_PropertieFile;
+
+import config.ConfigProperties;
+import dto.JwtResponse;
+import dto.LoginRequest;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+
+import static io.restassured.RestAssured.given;
+
+public class HelperWithPropertieFile {
+
+    public static RequestSpecification createBasicRequestSpecification() {
+        return new RequestSpecBuilder()
+                .setBaseUri(ConfigProperties.getProperty("baseUri"))
+                .setPort(ConfigProperties.getIntProperty("port"))
+                .setBasePath(ConfigProperties.getProperty("basePath"))
+                .addHeader("Content-Type", "application/json")
+                .build();
+    }
+
+    public static String getToken(RequestSpecification requestSpecification,String username, String password) {
+        LoginRequest loginRequest = new LoginRequest();
+        loginRequest.setUsername(username);
+        loginRequest.setPassword(password);
+
+        return given().spec(requestSpecification)
+                .body(loginRequest)
+                .when()
+                .post("/auth/signin")
+                .then()
+                .assertThat().statusCode(200)
+                .extract().response().as(JwtResponse.class).getToken();
+    }
+
+    public static RequestSpecification createAuthRequestSpecification(String token) {
+        return given().spec(createBasicRequestSpecification())
+                .header("Authorization", "Bearer " + token);
+    }
+
+    public static String getAdminToken(RequestSpecification requestSpecification) {
+        return getToken(requestSpecification,ConfigProperties.getProperty("adminUsername"),ConfigProperties.getProperty("adminPassword"));
+    }
+
+    public static String getUserToken(RequestSpecification requestSpecification) {
+        return getToken(requestSpecification,ConfigProperties.getProperty("userUsername"),ConfigProperties.getProperty("userPassword"));
+    }
+
+}
