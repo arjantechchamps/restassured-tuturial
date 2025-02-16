09c134d8440fdd4b299aa8b21490afa6f0913965
arjanassink <arjanassink@gmail.com>
2025-02-16 12:06:55 +0100

    create Builder

---
+++ tutorial/src/main/java/io/techchamps/tutorial/Builders/AdressRequestBuilder.java
@@ -0,0 +1,50 @@
+package io.techchamps.tutorial.Builders;
+
+import generated.dtos.AddressRequest;
+
+public class AdressRequestBuilder {
+
+        private final AddressRequest addressRequest;
+
+        public  AdressRequestBuilder() {
+            addressRequest = new AddressRequest();
+            addressRequest.setZipcode("1111AA");
+            addressRequest.setHouseNumber("5");
+            addressRequest.setCity("Enschede");
+            addressRequest.setStreet("Some street");
+            addressRequest.setCountry("Netherlands");
+        }
+
+        // With method to override zipcode.
+        public AdressRequestBuilder withZipCode(String zipCode) {
+            addressRequest.setZipcode(zipCode);
+            return this;
+        }
+
+        public AdressRequestBuilder withHouseNumber(String houseNumber){
+            addressRequest.setHouseNumber(houseNumber);
+            return this;
+        }
+
+        public AdressRequestBuilder withCity(String city){
+            addressRequest.setCity(city);
+            return this;
+        }
+
+        public AdressRequestBuilder withStreet(String street){
+            addressRequest.setStreet(street);
+            return this;
+        }
+
+        public AdressRequestBuilder withCountry(String country){
+            addressRequest.setCountry(country);
+            return this;
+        }
+
+
+        // Build the AddresRequest object.
+        public AddressRequest build() {
+            return addressRequest;
+        }
+
+    }
